d42fb53839875cfd29733d15747f71ad208df2c7
Stephen Schaub <sschaub@bju.edu>
2022-01-19 11:11:55 -0500

    Update

--- project3/_project3.sh
+++ project3/_project3.sh
@@ -9,6 +9,8 @@ require-files  --test-message "Compile produced rot47" rot47
 
 run-program --test-message "install.sh runs without error" sudo bash install.sh
 
-run-program --test-message "rot47 runs without error" --showoutputonpass  ./rot47 "The Quick Brown Fox Jumps Over The Lazy Dog."
+run-program --test-message "rot47 runs without error" --showoutputonpass  ./rot47 '"The Quick Brown Fox Jumps Over The Lazy Dog."'
+
+run-program --test-message "rot47 runs without error with keys" --showoutputonpass  ./rot47 '"Fox KEY:01 Fox Fox KEY:02 Fox"'
 
 require-files  --test-message "Report submitted" REPORT.md 
